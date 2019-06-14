77c0c70183503af73066b2f7762a66a0c239959f
Max Kramer <max@maxkramer.co>
2019-06-14 23:58:17 +0200

    Initial commit

---
+++ src/main/kotlin/co/maxkramer/kotlinstarter/KotlinStarterApplication.kt
@@ -0,0 +1,11 @@
+package co.maxkramer.kotlinstarter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinStarterApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinStarterApplication>(*args)
+}
---
+++ src/test/kotlin/co/maxkramer/kotlinstarter/KotlinStarterApplicationTests.kt
@@ -0,0 +1,16 @@
+package co.maxkramer.kotlinstarter
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinStarterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
