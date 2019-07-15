8e631b9a5ed743785c1821eb656bddd496ae70c7
Max Kramer <max@maxkramer.co>
2019-07-15 12:17:59 +0200

    Updates dependencies and runs gradle task as non-root user

--- src/test/kotlin/co/maxkramer/kotlinstarter/KotlinStarterApplicationTests.kt
+++ src/test/kotlin/co/maxkramer/kotlinstarter/KotlinStarterApplicationTests.kt
@@ -1,12 +1,14 @@
 package co.maxkramer.kotlinstarter
 
-import org.junit.Test
+import co.maxkramer.kotlinstarter.support.UseEmbeddedPostgres
+import org.junit.jupiter.api.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
 @SpringBootTest
+@RunWith(SpringRunner::class)
+@UseEmbeddedPostgres
 class KotlinStarterApplicationTests {
     @Test
     fun contextLoads() {
