583c46210554acf8f4b9aa7b62f370333fd1cd63
Nico Sorger <nico.sorger@bcgdv.com>
2019-11-22 16:45:58 +0100

    Remove JUnit4 Spring Runner

--- src/test/kotlin/co/maxkramer/kotlinstarter/KotlinStarterApplicationTests.kt
+++ src/test/kotlin/co/maxkramer/kotlinstarter/KotlinStarterApplicationTests.kt
@@ -2,12 +2,9 @@ package co.maxkramer.kotlinstarter
 
 import co.maxkramer.kotlinstarter.support.UseEmbeddedPostgres
 import org.junit.jupiter.api.Test
-import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
 @SpringBootTest
-@RunWith(SpringRunner::class)
 @UseEmbeddedPostgres
 class KotlinStarterApplicationTests {
     @Test
